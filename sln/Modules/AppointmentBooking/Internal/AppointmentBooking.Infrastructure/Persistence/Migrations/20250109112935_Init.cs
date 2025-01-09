a9d20d921515b1e564bb4d1d4efd19d40547ac0c
Ibrahim Megahed <Ibrahim__Hasan@outlook.com>
2025-01-09 14:30:29 +0300

    Add Migration

---
+++ sln/Modules/AppointmentBooking/Internal/AppointmentBooking.Infrastructure/Persistence/Migrations/20250109112935_Init.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppointmentBooking.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "AppointmentBooking");
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                schema: "AppointmentBooking",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SlotId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PatientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PatientName = table.Column<string>(type: "TEXT", nullable: false),
+                    ReservedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments",
+                schema: "AppointmentBooking");
+        }
+    }
+}
